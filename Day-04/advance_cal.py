d2a2969d8f7b2e3c44d23bee4fa528bb60563151
sudhamsh <sudhamsh1996@gmail.com>
2023-10-26 19:36:25 +0530

    added the virtual environment stuff

---
+++ Day-04/advance_cal.py
@@ -0,0 +1,12 @@
+# collection of functions is called module and collection of modules is called package/library
+# if your code is written in multiple python files, we can bundle these files into a package/library
+# this is the modular approach
+# we can use the desired function as well as all the functions
+# we can also import one particular module from a functions
+# import demo as arbitary
+# arbitary #it calls all the modules in a file
+# arbitary.add() # it calls the add function in demo.py
+
+import demo
+# demo
+demo.add() #we get all the stuff because in demo function, we are invoking all the functions and printing the functions
---
+++ Day-04/demo.py
@@ -0,0 +1,18 @@
+def add(a, b):
+    c = a + b
+    return c
+addition = add(2, 4)
+def sub(a, b):
+    c = a - b
+    return c
+def mul(a, b):
+    c = a * b
+    return c
+def div(a, b):
+    c = a % b
+    return c
+print(add(2, 4))
+print(addition)
+print(sub(2, 4))
+print(mul(2, 4))
+print(div(2, 4))
