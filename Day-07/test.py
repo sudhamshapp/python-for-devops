9fdfc4b977df069eedfc0282c8c7dc7c746188b4
sudhamsh <sudhamsh1996@gmail.com>
2023-11-02 10:02:27 +0530

    added a file

--- Day-07/test.py
+++ Day-07/test.py
@@ -1,5 +1,6 @@
 import sys
 type = sys.argv[1]
+# print(f"This gonna give us first item of argument - {sys.argv[0]}")
 if type == "t2.micro": # == comparison operator
     print("it will charge you $2 a day")
 elif type == "t2.medium":
