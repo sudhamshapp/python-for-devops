d29213a29f803ed90836838268241081c7f02b01
sudhamsh <sudhamsh1996@gmail.com>
2023-11-09 21:15:40 +0530

    added loops concept

---
+++ Day-09/demo.py
@@ -0,0 +1,9 @@
+# difference b/w for(definite number of times) and while loop(dynamically until the condition is true)
+
+# Repetitive action on block of code, whether it's one ormultiple lines
+for i in range(10):
+    # print(i)
+    print("sudhamsh")
+
+# print(type(range(10)))
+print(range(10))
