4b87d96d723ced1beffd77636d41b3ea8a332ac0
sudhamsh <sudhamsh1996@gmail.com>
2024-10-02 22:23:02 +0530

    added control and break statements for the loops

--- Day-05/reference.py
+++ Day-05/reference.py
@@ -10,6 +10,6 @@ print("==============================================")
 def add_numbers(a, b):
     sum_value = a + b
     return sum_value
-add_numbers(4, 6)
+# add_numbers(4, 6)
 sum_stuff = add_numbers(4,4)
 print(f"This is the sum_stuff result: ", {sum_stuff})    
---
+++ Day-09/sample.py
@@ -0,0 +1,14 @@
+# for i in range(10):
+#     print(i, "amd")
+
+
+# mars  = [1,2,3,4,5,6,7,8,9,10]
+# for i in mars:
+#     print(i)
+
+mars  = [1,2,3,4,'sudhamsh',5,6,7,8,9,10]
+for i in mars:
+    if i == 'sudhamsh':
+        # continue
+        break
+    print(i)
